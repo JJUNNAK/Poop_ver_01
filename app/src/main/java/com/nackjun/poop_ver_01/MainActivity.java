80befe306914d634c786718d2bfafe611768cdda
choinackjun <stonechoiqwe@naver.com>
2022-07-23 03:43:29 +0900

    리스트뷰 구현 데이터 입력 전.

--- app/src/main/java/com/nackjun/poop_ver_01/MainActivity.java
+++ app/src/main/java/com/nackjun/poop_ver_01/MainActivity.java
@@ -14,16 +14,28 @@ public class MainActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //상세보기 버튼 클릭시 화면전환
+        Button show_data_btn = (Button) findViewById(R.id.show_data_btn); //버튼 아이디
+        show_data_btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){                            //연결할 엑티비티
+                Intent intent = new Intent(getApplicationContext(),ListviewActivity.class);
+                startActivity(intent);
+            }
+        });
 
-    //노선보기 버튼 클릭시 화면전환
-    Button show_line_btn = (Button) findViewById(R.id.show_line_btn); //버튼 아이디
+        //노선보기 버튼 클릭시 화면전환
+        Button show_line_btn = (Button) findViewById(R.id.show_line_btn); //버튼 아이디
         show_line_btn.setOnClickListener(new View.OnClickListener(){
-        @Override
-        public void onClick(View view){                            //연결할 엑티비티
-            Intent intent = new Intent(getApplicationContext(),Line_Menu.class);
-            startActivity(intent);
-        }
-    });
+            @Override
+            public void onClick(View view){                            //연결할 엑티비티
+                Intent intent = new Intent(getApplicationContext(),Line_Menu.class);
+                startActivity(intent);
+            }
+        });
+
+
+
 
     }
 
